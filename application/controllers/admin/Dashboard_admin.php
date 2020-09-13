d35f4fccd8a265629ca2d2a3abab8f163280b093
rinayu30 <rinayxiipa1@gmail.com>
2020-09-13 09:18:41 +0700

    ubah warns

--- application/controllers/admin/Dashboard_admin.php
+++ application/controllers/admin/Dashboard_admin.php
@@ -6,14 +6,17 @@ class Dashboard_admin extends CI_Controller
     {
         parent::__construct();
         check_not_login();
+        $this->load->model(['penjualan_model', 'produk_model', 'bahan_masuk_model', 'auth_model']);
     }
 
-    public function index(){
-       
+    public function index()
+    {
+        // $data['jml_penjualan'] = $this->penjualan_model->getTotalPenjualan();
+        // $data['jml_pembelian'] = $this->bahan_masuk_model->getTotalPembelian();
+
         $this->load->view('templates_adm/header');
         $this->load->view('templates_adm/sidebar');
         $this->load->view('admin/dashboard');
         $this->load->view('templates_adm/footer');
-       
     }
 }
